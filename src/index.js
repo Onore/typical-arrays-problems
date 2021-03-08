e48c9e10fdd9bb00316f19ae1613e619e100e158
Onore <job.kyiv@gmail.com>
2021-03-08 21:52:15 +0200

    feat: complite functions code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
+exports.min = function min (arr) {
+  if( Array.isArray(arr) && arr.length > 0) {
+    return arr.sort( (prev, next) => prev - next)[0];
+  }
+
   return 0;
 }
 
-exports.max = function max (array) {
+exports.max = function max (arr) {
+  if( Array.isArray(arr) && arr.length > 0) {
+    return arr.sort( (prev, next) => next - prev)[0];
+  }
+
   return 0;
 }
 
-exports.avg = function avg (array) {
+exports.avg = function avg (arr) {
+  if( Array.isArray(arr) && arr.length > 0) {
+    return arr.reduce( (acc, val) => acc = acc + val) / arr.length;
+  }
+
   return 0;
 }
